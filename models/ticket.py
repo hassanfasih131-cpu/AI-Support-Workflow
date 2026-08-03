92fe4900db992967006ca1b2e435e8240a61bb68
Hassan Fasih <hassan.fasih131@gmail.com>
2026-08-03 11:17:31 -0700

    Add Ticket Base class

---
+++ models/ticket.py
@@ -0,0 +1,33 @@
+#Build Ticket Base Class
+from datetime import datetime
+class Ticket: #blueprint
+    def __init__(self, ticket_id,
+                 customer_name, message, category,
+                 priority):
+        self.ticket_id = ticket_id
+        self.customer_name = customer_name
+        self.message=message
+        self.category=category
+        self.priority=priority
+        self.status="Open"
+        self.creation_time=datetime.now()
+        self.notes=[]
+    def __str__(self):
+        return (f"Ticket ID: {self.ticket_id} \n" f"Customer Name: {self.customer_name}\n"
+                f"Message: {self.message}\n" f"Category: {self.category}\n"
+                f"Priority: {self.priority}\n" f"Status: {self.status}\n" 
+                f"Creation Time: {self.creation_time}" f"Notes: {self.notes}")
+    def addnote(self,note):
+        self.notes.append(note)
+    def update_status(self,status):
+        allowed_status=["Open","Assigned","In Progress","Resolved","Escalated"]
+        if status not in allowed_status:
+            print("Invalid Status")
+        else:
+            self.status = status
+    def change_priority(self,priority):
+        allowed_priority=["Urgent","High","Medium","Low"]
+        if priority not in allowed_priority:
+            print('Invalid Priority')
+        else:
+            self.priority = priority
