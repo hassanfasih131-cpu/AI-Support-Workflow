3c994a46a69b75cf6ef995e00ecb3000537d8faf
Hassan Fasih <hassan.fasih131@gmail.com>
2026-08-05 04:10:29 -0700

    Added a way to protect ticket state via encapsulation

--- models/ticket.py
+++ models/ticket.py
@@ -1,4 +1,6 @@
-#Build Ticket Base Class
+#Build Ticket Base Class and Encapsulation
+#Ticket base class is created to store data
+#Status and Priority are encapsulated using ._ to prevent random data
 from datetime import datetime
 class Ticket: #blueprint
     def __init__(self, ticket_id,
@@ -8,29 +10,31 @@ class Ticket: #blueprint
         self.customer_name = customer_name
         self.message=message
         self.category=category
-        self.priority=priority
-        self.status="Open"
+        self._priority=priority
+        self._status="Open"
         self.creation_time=datetime.now()
         self.notes=[]
     def __str__(self):
         return (f"Ticket ID: {self.ticket_id} \n" f"Customer Name: {self.customer_name}\n"
                 f"Message: {self.message}\n" f"Category: {self.category}\n"
-                f"Priority: {self.priority}\n" f"Status: {self.status}\n" 
+                f"Priority: {self._priority}\n" f"Status: {self._status}\n" 
                 f"Creation Time: {self.creation_time}\n" f"Notes: {self.notes}")
+    #Adds a note in []
     def addnote(self,note):
         self.notes.append(note)
+    #Updates the status of the ticket
     def update_status(self,status):
-        allowed_status=["Open","Assigned","In Progress","Resolved"]
+        allowed_status=["Open","Assigned","In Progress","Resolved","Escalated"]
         if status not in allowed_status:
             print("Invalid Status")
         else:
-            self.status = status
+            self._status = status
     def change_priority(self,priority):
         allowed_priority=["Urgent","High","Medium","Low"]
         if priority not in allowed_priority:
             print('Invalid Priority')
         else:
-            self.priority = priority
+            self._priority = priority
 
 #Create Specialised ticket types
 class BillingTicket(Ticket):
@@ -48,3 +52,16 @@ class AccountTicket(Ticket):
 class GeneralTicket(Ticket):
     def __init__(self,ticket_id,customer_name,message,priority):
         super().__init__(ticket_id,customer_name,message,"General",priority)
+
+#TEST
+
+'''from models.ticket import TechnicalTicket
+
+ticket1=TechnicalTicket(1,"Manahil","NULL","High") #creates a ticket
+ticket1.update_status("Resolved") #status is updated 
+ticket1.change_priority("Urgent") #Priority is updated
+print(ticket1,"\n\n")
+
+ticket1.update_status("nusnrfuw") #attempt to update status with incorrect data
+ticket1.change_priority("DVNJISNV") #attempt to update priority with incorrect data
+print(ticket1)'''
